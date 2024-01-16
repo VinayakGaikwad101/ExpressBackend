744564d5e115079f6610334bb6ac2495cf10c2ff
VinayakGaikwad <vinaayakgaikwad@gmail.com>
2024-01-16 22:42:54 +0530

    tested res.json

--- index.js
+++ index.js
@@ -20,6 +20,25 @@ app.get("/nigga", (req, res) => {
   res.send("Hi my nigs");
 });
 
+const myData = {
+  "first user": {
+    name: "example text",
+    "roll number": "19380123",
+    password: "adhasdjhas",
+    address: "asjdakdshaksd",
+  },
+  "second user": {
+    name: "example text3",
+    "roll number": "193824320123",
+    password: "adhasdjha1231s",
+    address: "asjdakdshaksadas3333d",
+  },
+};
+
+app.get("/myData", (req, res) => {
+  res.json(myData);
+});
+
 app.listen(process.env.MY_PORT, () => {
   console.log(`Server is listening on port ${process.env.MY_PORT}`);
 });
